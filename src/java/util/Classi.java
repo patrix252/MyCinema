a36e5c2c69a6e22f05d3430e0a60db10d862383a
Tentello <prempaolo@gmail.com>
2016-01-14 15:46:48 +0100

    Sistemato anche codice QrCode, manca ancora la parte admin

--- src/java/util/Classi.java
+++ src/java/util/Classi.java
@@ -26,6 +26,19 @@ import javax.mail.internet.*;
 import javax.activation.*;
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpSession;
+import javax.imageio.ImageIO;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Hashtable;
 /**
  *
  * @author Paolo
@@ -62,27 +75,43 @@ public class Classi {
         String absoluteDiskPath = context.getRealPath(relativeWebPath);
         File file = new File(absoluteDiskPath);
         try {
-            new Classi().createPDF(absoluteDiskPath, session);
+            new Classi().createPDF(absoluteDiskPath, session, context);
         } catch (DocumentException | IOException ex) {
             Logger.getLogger(Classi.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
     
-    public void createPDF(String dest, HttpSession session) throws DocumentException, IOException {
+    public void createPDF(String dest, HttpSession session, ServletContext context) throws DocumentException, IOException {
         
         Document document = new Document();
         Utente utente = (Utente)session.getAttribute("utente");
         Spettacolo spett = (Spettacolo)session.getAttribute("spettacoloPrenotazione");
         Film film = (Film)session.getAttribute("film");
         List<Posto> postiInteri = (List<Posto>)session.getAttribute("postiInteri");
+        String pi = "";
+        for(int i=0; i<postiInteri.size(); i++){
+            pi = pi.concat(postiInteri.get(i).getRiga() + "-" + postiInteri.get(i).getColonna()+" ");
+        }
         List<Posto> postiRidotti = (List<Posto>)session.getAttribute("postiRidotti");
+        String pr = "";
+        for(int i=0; i<postiRidotti.size(); i++){
+            pr = pr.concat(postiRidotti.get(i).getRiga() + "-" + postiRidotti.get(i).getColonna()+" ");
+        }
         PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream(dest));
         document.open();
         LineSeparator UNDERLINE = new LineSeparator(1, 100, null, Element.ALIGN_CENTER, -2);
         //CARICA IL QRCODE IN FORMATO PNG SUL PDF
-        //Image qrcode = Image.getInstance("qrcode.png");
-        //document.add(qrcode);
-        //qrcode.scaleAbsolute(20,20);
+        String testoQR = utente.getNome()+" "+utente.getCognome()+"\n"+
+                         film.getTitolo()+"\n"+
+                         "MyCinema\n" +
+                         spett.getData() + " " + spett.getOra() + "\n" +
+                         "posti ridotti: " + postiRidotti.size() + "\n" +
+                         pr + "\n" +
+                         "posti interi: " + postiInteri.size() + "\n" +
+                         pr + "\n";                     
+        Image qrcode = Image.getInstance(createQrCode(context, testoQR));
+        document.add(qrcode);
+        qrcode.scaleAbsolute(20,20);
         document.add(new Paragraph("    Nome Utente: " + utente.getNome(), FontFactory.getFont(FontFactory.TIMES, 19)));
         document.add(new Paragraph("    Cognome Utente: " + utente.getCognome(), FontFactory.getFont(FontFactory.TIMES, 19)));
         document.add(new Paragraph("    Titolo Film: " + film.getTitolo(), FontFactory.getFont(FontFactory.TIMES, 19)));
@@ -90,26 +119,13 @@ public class Classi {
         document.add(new Paragraph("    Orario Proiezione: " + spett.getOra() , FontFactory.getFont(FontFactory.TIMES, 19)));
         document.add(new Paragraph("    Prezzo totale: " + session.getAttribute("totale") + "€", FontFactory.getFont(FontFactory.TIMES, 19)));
         document.add(new Paragraph("    Tipologia di Biglietto: ridotti " + postiRidotti.size(), FontFactory.getFont(FontFactory.TIMES, 19)));
-        for(int i=0; i<postiRidotti.size(); i++){
-            document.add(new Paragraph("    Posto: " + postiRidotti.get(i).getRiga() + " " + postiRidotti.get(i).getColonna(), FontFactory.getFont(FontFactory.TIMES, 19)));
-        }
+        document.add(new Paragraph("    Posti: " + pr, FontFactory.getFont(FontFactory.TIMES, 19)));
         document.add(new Paragraph("    Tipologia di Biglietto: interi " + postiInteri.size(), FontFactory.getFont(FontFactory.TIMES, 19)));
-        for(int i=0; i<postiInteri.size(); i++){
-            document.add(new Paragraph("    Posto: " + postiInteri.get(i).getRiga() + " " + postiInteri.get(i).getColonna(), FontFactory.getFont(FontFactory.TIMES, 19)));
-        }
-        document.add(new Paragraph(" "));
-        document.add(new Paragraph(" "));
-        document.add(new Paragraph(" "));
-        document.add(new Paragraph(" "));
-        document.add(new Paragraph(" "));
-        document.add(new Paragraph(" "));
-        document.add(new Paragraph(" "));
-        document.add(new Paragraph(" "));
+        document.add(new Paragraph("    Posti: " + pi, FontFactory.getFont(FontFactory.TIMES, 19)));
         document.add(new Paragraph(" "));
         document.add(new Paragraph(" "));
         document.add(new Paragraph("    Grazie per aver acquistato il tuo biglietto online con noi!", FontFactory.getFont(FontFactory.HELVETICA, 15)));
         document.add(new Paragraph("    Stampa questo pdf, portalo con te al cinema e goditi il film!", FontFactory.getFont(FontFactory.HELVETICA, 15)));
-        document.add(new Paragraph(" "));
         document.add(new Paragraph(" "));
         document.add(new Paragraph(" "));
         //FOOTER, DA NON TOCCARE!
@@ -161,6 +177,46 @@ public class Classi {
         }catch (MessagingException ex) {
             ex.printStackTrace();
         }
+    }
+    
+    public String createQrCode(ServletContext context, String myCodeText){
+        
+        // change path as per your laptop/desktop location
+        String relativeWebPath = "/QrCode.png";
+        String absoluteDiskPath = context.getRealPath(relativeWebPath);
+        int size = 256;
+        String fileType = "png";
+        File myFile = new File(absoluteDiskPath);
+        try {
+            Hashtable<EncodeHintType, ErrorCorrectionLevel> hintMap = new Hashtable<EncodeHintType, ErrorCorrectionLevel>();
+            hintMap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L);
+            QRCodeWriter qrCodeWriter = new QRCodeWriter();
+            BitMatrix byteMatrix = qrCodeWriter.encode(myCodeText,BarcodeFormat.QR_CODE, size, size, hintMap);
+            int CrunchifyWidth = byteMatrix.getWidth();
+            BufferedImage image = new BufferedImage(CrunchifyWidth, CrunchifyWidth,
+                    BufferedImage.TYPE_INT_RGB);
+            image.createGraphics();
+ 
+            Graphics2D graphics = (Graphics2D) image.getGraphics();
+            graphics.setColor(Color.WHITE);
+            graphics.fillRect(0, 0, CrunchifyWidth, CrunchifyWidth);
+            graphics.setColor(Color.BLACK);
+ 
+            for (int i = 0; i < CrunchifyWidth; i++) {
+                for (int j = 0; j < CrunchifyWidth; j++) {
+                    if (byteMatrix.get(i, j)) {
+                        graphics.fillRect(i, j, 1, 1);
+                    }
+                }
+            }
+            ImageIO.write(image, fileType, myFile);
+        } catch (WriterException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("\n\nYou have successfully created QR Code.");
+        return absoluteDiskPath;
     }
 
 }
